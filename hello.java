480987e2969070b144eb92502067ae65f8d8fee0
kishorenath-ceg <saravanankishor679@gmail.com>
2026-09-22 20:45:26 +0530

    Test Jenkins trigger

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class Hello{
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins - Trigger Test!");
+        System.out.println("Hello from  - Trigger Test!");
     }
 }
